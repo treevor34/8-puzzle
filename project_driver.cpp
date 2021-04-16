14436359ea7622c73ead7ce0202a5e2b9aac211e
treevor34 <46980500+treevor34@users.noreply.github.com>
2021-04-16 13:28:37 -0400

    Upload
    Just deleted my mess of comments and nodes

---
+++ project_driver.cpp
@@ -0,0 +1,169 @@
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <sstream>
+using namespace std;
+//#include "class_node.h"
+/*
+struct node {
+    vector<int> puzzle;
+    node* left;
+    node* up;
+    node* right;
+    node* down;
+    node* parent; 
+};
+*/
+void print(vector<int> vec)
+{
+    for(int i = 0; i < vec.size(); i++)
+    {
+        cout << vec.at(i) << " ";
+        if((i+1) % 3 == 0)
+            cout << endl;
+    }
+}
+
+bool isSame(vector<int> un, vector<int> dos)
+{
+    for(int i = 0; i < 9; i++)
+    {
+        if(un.at(i) == dos.at(i))
+            continue;
+        else
+            return false;
+    }
+    return true;
+}
+
+int findZero(vector<int> tmp)
+{
+    cout << "Find zero" << endl;
+    for(int i = 0; i < 9; i++)
+        if(tmp.at(i) == 0)
+            return i;
+    cout << "No 0 piece of the puzzle." << endl;
+    exit(0);
+}
+
+vector<int> move(vector<int> tmpy, int exp)
+{
+    cout << "Move:" << exp << endl;
+    vector<int> tmp = tmpy;
+    print(tmp);
+    int swapped, num = findZero(tmp);
+    vector<int> empty;
+    switch(exp) {
+        case 0: //left
+            cout << "left" << endl;
+            if(num == 0 || num == 3 || num == 6)
+                return empty;
+            
+            //records the value being swapped left with
+            swapped = tmp.at(num - 1);
+
+            //swapped the values
+            tmp.at(num - 1) = 0;
+            tmp.at(num) = swapped;
+            return tmp;
+            break;
+        case 1: //up
+            cout << "up" << endl;
+            if(num == 0 || num == 1 || num == 2)
+                return empty;
+            cout << "past the up" << endl;
+            //records the value being swapped left with
+            swapped = tmp.at(num - 3);
+
+            //swapped the values
+            tmp.at(num - 3) = 0;
+            tmp.at(num) = swapped;
+            return tmp;
+            break;
+        case 2: //right
+            cout << "right" << endl;
+            if(num == 2 || num == 5 || num == 8)
+                return empty;
+            
+            //records the value being swapped left with
+            swapped = tmp.at(num + 1);
+
+            //swapped the values
+            tmp.at(num + 1) = 0;
+            tmp.at(num) = swapped;
+            print(tmp);
+            return tmp;
+            break;
+        case 3://down
+            cout << "down" << endl;
+            if(num == 6 || num == 7 || num == 8)
+                return empty;
+            
+            //records the value being swapped left with
+            swapped = tmp.at(num + 3);
+
+            //swapped the values
+            tmp.at(num + 3) = 0;
+            tmp.at(num) = swapped;
+            return tmp;
+            break;
+    }
+    cout << "This is wrong lol" << endl;
+    return tmp;
+}
+int bfs(vector<int> cur, vector<int> solution)
+{
+    int ret = -1;
+
+    queue<vector<int>> myqueue;
+
+    vector<int> tmp;
+
+    vector<int> confused = move(cur, 2);
+    print(confused);
+    cout << "print right"<< endl;
+    cout<< "confused" << endl;
+
+    //myqueue.push();
+    /*while(!myqueue.empty() && ret == -1)
+    {
+    }*/
+
+
+    return ret;
+}
+
+
+int main()
+{
+    string input;
+    vector<int> solution;
+    vector<int> start;
+    int z, tmp;
+    //should check for repeats but i have not yet
+    cout << "Values must range from 0-8, with only one occurence of each." << endl << "Please enter 9 seperate values for the puzzle start state:" << endl;
+    
+    for(z = 0; z < 9; z++)
+    {
+        cout << "Start value #"<< z+1 << ": ";
+        cin >> tmp;
+        start.push_back(tmp);
+    }
+
+    for(z = 0; z < 9; z++)
+    {
+        cout << "Solution value #"<< z+1 << ": ";
+        cin >> tmp;
+        solution.push_back(tmp);
+    }
+
+    cout << endl << "Starting State: "<< endl;
+    print(start);
+    cout << endl << "Solution State: "<< endl;
+    print(solution);
+     
+    cout << "Start bfs" << endl;
+    
+    //int answer = bfs(start, solution);
+    return 0;
+}
